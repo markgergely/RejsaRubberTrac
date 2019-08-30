aa373f75eed5a04896836651fd685ce3ba5c1f66
Mark Gergely <mark@majority.co>
2019-08-30 15:08:06 -0700

    re-factor TempSensor

--- main/temp_sensor.h
+++ main/temp_sensor.h
@@ -5,10 +5,12 @@
 
 class TempSensor {
 private:
+  float getPixelTemperature(uint8_t x, uint8_t y);
+  float calculateColumnTemperature(uint16_t column_content[], uint8_t size);
 #if FIS_SENSOR == FIS_MLX90621
-    MLX90621 FISDevice;
+  MLX90621 FISDevice;
 #elif FIS_SENSOR == FIS_MLX90640
-    MLX90640 FISDevice;
+  MLX90640 FISDevice;
 #endif
 public:
   int16_t measurement[FIS_X];
