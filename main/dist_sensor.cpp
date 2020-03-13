6909929992dcb298b425ca11b3987d9622243e21
Mark Gergely <mark@majority.co>
2020-03-12 21:03:49 -0700

    dist sensor gets a configurable 2wire interface, cleanup

--- main/dist_sensor.cpp
+++ main/dist_sensor.cpp
@@ -4,29 +4,32 @@
 #include "algo.h"
 #include <Wire.h>
 
-void DistSensor::initialise() {
+bool DistSensor::initialise(TwoWire *I2Cpipe, char *wheelPos) {
   distance = 0;
-  Wire.begin();
+  thisWire = I2Cpipe;
+  thisWheelPos = wheelPos;
+//  Wire.begin(GPIOSDA,GPIOSCL); // initialize I2C w/ I2C pins from config
   pinMode(GPIODISTSENSORXSHUT, OUTPUT);
   digitalWrite(GPIODISTSENSORXSHUT, LOW);
   delay(10);
   digitalWrite(GPIODISTSENSORXSHUT, HIGH);
   delay(10);
-  uint32_t temporary;
-  present = sensor.init();
+  present = sensor.init(true, thisWire);
   sensor.setTimeout(500);
-  sensor.setMeasurementTimingBudget(50000);
-  temporary = sensor.getMeasurementTimingBudget();
-  Serial.printf("wtf: %dmm\n", temporary);
   sensor.startContinuous();
+  return present;
 };
 
 void DistSensor::measure() {
   if (present) {
     int16_t rawDistance = sensor.readRangeContinuousMillimeters();
     if (rawDistance < 500) {
-       distance = distanceFilter(rawDistance) - DISTANCEOFFSET; // Only update distance if its less than 500mm
+       distance = distanceFilter(rawDistance) - DISTANCEOFFSET; // Only update distance if it is less than 500mm
     }
-    if (sensor.timeoutOccurred()) { Serial.println("Distance sensor TIMEOUT!"); }
+    if (sensor.timeoutOccurred()) {
+      Serial.print("TIMEOUT: Distance sensor for ");
+      Serial.print(thisWheelPos);
+      Serial.print("!\n");
+    }
   }
 }
